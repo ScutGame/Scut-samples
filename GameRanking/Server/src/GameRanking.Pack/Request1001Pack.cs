f9041b0b0b1aec1a7eb3fb97dd6dcea85d749777
ScutGame <wzf_88@qq.com>
2014-07-31 19:49:32 +0800

    add scut samples

---
+++ GameRanking/Server/src/GameRanking.Pack/Request1001Pack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ProtoBuf;
+
+namespace GameRanking.Pack
+{
+    [ProtoContract]
+    public class Request1001Pack
+    {
+        [ProtoMember(101)]
+        public int PageIndex { get; set; }
+
+        [ProtoMember(102)]
+        public int PageSize { get; set; }
+    }
+}
---
+++ GameRanking/Server/src/GameRanking.Pack/Response1001Pack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using ProtoBuf;
+
+namespace GameRanking.Pack
+{
+    [ProtoContract]
+    public class Response1001Pack
+    {
+        public Response1001Pack()
+        {
+            Items = new List<RankData>();
+        }
+
+        [ProtoMember(101)]
+        public int PageCount { get; set; }
+
+        [ProtoMember(102)]
+        public List<RankData> Items { get; set; }
+    }
+
+    [ProtoContract]
+    public class RankData
+    {
+
+        [ProtoMember(101)]
+        public string UserName { get; set; }
+
+        [ProtoMember(102)]
+        public int Score { get; set; }
+    }
+}
